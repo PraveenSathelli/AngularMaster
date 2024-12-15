e5f579c1ad4e5d56fb9159c883495eb58a467fbc
Praveen Sathelli <praveensathelli11@gmail.com>
2024-12-15 20:47:33 +0530

    43 *ngFor old version which is now imported from common lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent } from './header/header.component';
 import { DUMMY_USERS } from '../dummy-users';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports:[HeaderComponent,UserComponent,TasksComponent]
+  imports:[HeaderComponent,UserComponent,TasksComponent,NgFor]
 })
 export class AppComponent {
   title = 'Members';
